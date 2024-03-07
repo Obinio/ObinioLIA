60292b0223f00cc7f83868e8bf415d47b2648b9a
JacobErc <jacob.ercan@obinio.se>
2024-03-07 12:30:14 +0100

    fixat homepage

---
+++ frontend/obiniolia/src/components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div classname="homePage">
+      <h1>Välkommen till Dokumenthanteraren!</h1>
+      <p>Detta är startsidan för din applikation.</p>
+    </div>
+  );
+}
+
+export default HomePage;
