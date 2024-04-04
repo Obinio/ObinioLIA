d87d890a1ac816a43cb6e2dcf8cfbdc7f093c8c0
JacobErc <jacob.ercan@obinio.se>
2024-04-04 12:30:38 +0200

    Gjort strings nullable efter massa varningar

--- backend/Models/Document.cs
+++ backend/Models/Document.cs
@@ -3,8 +3,8 @@ namespace backend.Models
     public class Document
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
         // Du kan lägga till fler egenskaper här, t.ex. datum, författare, etc.
     }
 }
