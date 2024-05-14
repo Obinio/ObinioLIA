6949e5884472904f839806db8c512fcac77b85a4
JacobErc <jacob.ercan@obinio.se>
2024-05-14 18:13:03 +0200

    uppdaterad

--- frontend/obiniolia/src/components/AuthContext.js
+++ frontend/obiniolia/src/components/AuthContext.js
@@ -2,7 +2,6 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext(null);
 
-
 export function useAuth() {
   return useContext(AuthContext);
 }
@@ -11,19 +10,30 @@ export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
-    const user = localStorage.getItem('userToken');
-    if (user) {
-      setCurrentUser(user);
+    const userToken = localStorage.getItem('userToken');
+    // Här kan du också lägga till ytterligare kontroller om du lagrar mer information, t.ex. userRole
+    if (userToken) {
+      setCurrentUser({
+        token: userToken,
+        // Exempel: Lägg till role om du sparar detta också
+        role: localStorage.getItem('userRole')
+      });
     }
   }, []);
 
-  const login = (token) => {
+  const login = (token, role) => {
     localStorage.setItem('userToken', token);
-    setCurrentUser(token);
+    localStorage.setItem('userRole', role);  // Spara rollen om du använder den
+    setCurrentUser({
+      token: token,
+      role: role
+    });
   };
 
   const logout = () => {
+    // Ta bort all användarinformation från localStorage
     localStorage.removeItem('userToken');
+    localStorage.removeItem('userRole');  // Se till att ta bort rollen också
     setCurrentUser(null);
   };
 
